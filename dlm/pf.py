bd86f548a1a3b0750fae504369cf81282723a3bb
gcgibson <gcgibson@1x-nat-vl931-172-30-166-126.wireless.umass.edu>
2017-11-17 13:05:23 -0500

    added KCDE

---
+++ dlm/pf.py
@@ -0,0 +1,51 @@
+from numpy.random import uniform
+import numpy as np
+
+
+def create_uniform_particles(x_range, y_range, hdg_range, N):
+    particles = np.empty((N, 3))
+    particles[:, 0] = uniform(x_range[0], x_range[1], size=N)
+    particles[:, 1] = uniform(y_range[0], y_range[1], size=N)
+    particles[:, 2] = uniform(hdg_range[0], hdg_range[1], size=N)
+    particles[:, 2] %= 2 * np.pi
+    return particles
+
+
+def predict(particles, u, std, dt=1.):
+    """ move according to control input u (heading change, velocity)
+    with noise Q (std heading change, std velocity)`"""
+
+    N = len(particles)
+    # update heading
+    particles[:, 2] += u[0] + (randn(N) * std[0])
+    particles[:, 2] %= 2 * np.pi
+
+    # move in the (noisy) commanded direction
+    dist = (u[1] * dt) + (randn(N) * std[1])
+    particles[:, 0] += np.cos(particles[:, 2]) * dist
+    particles[:, 1] += np.sin(particles[:, 2]) * dist
+
+
+
+def update(particles, weights, z, R, landmarks):
+    weights.fill(1.)
+    for i, landmark in enumerate(landmarks):
+        distance = np.linalg.norm(particles[:, 0:2] - landmark, axis=1)
+        weights *= scipy.stats.norm(distance, R).pdf(z[i])
+
+    weights += 1.e-300      # avoid round-off to zero
+    weights /= sum(weights) # normalize
+
+def estimate(particles, weights):
+    """returns mean and variance of the weighted particles"""
+
+    pos = particles[:, 0:2]
+    mean = np.average(pos, weights=weights, axis=0)
+    var  = np.average((pos - mean)**2, weights=weights, axis=0)
+    return mean, var
+
+
+
+particles = create_uniform_particles((0,1), (0,1), (0, 5), 1000)
+weights = np.array([.25]*1000)
+print (estimate(particles, weights))
